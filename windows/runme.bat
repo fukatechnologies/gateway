5a8de05a258b5999eadeab71b89e82be0ab99823
fukatechnologies <67410073+fukatechnologies@users.noreply.github.com>
2020-07-19 17:07:58 +0800

    Update runme.bat

--- windows/runme.bat
+++ windows/runme.bat
@@ -1,1 +1,2 @@
-start https://raw.githubusercontent.com/fukatechnologies/gateway/master/windows/runme.bat
+set url="https://raw.githubusercontent.com/fukatechnologies/gateway/master/windows/runme.bat"
+curl %url% -o runme.bat
