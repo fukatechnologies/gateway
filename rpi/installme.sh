69e07cb258f33e6c8ed63bd926a1977c6389cd4e
fukatechnologies <67410073+fukatechnologies@users.noreply.github.com>
2020-10-01 19:07:53 +0800

    Update installme.sh

--- rpi/installme.sh
+++ rpi/installme.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+echo "Delete everthing in folder"
+rm -r *
+echo ""
 echo "Downloading to local folder"
 curl -k https://raw.githubusercontent.com/fukatechnologies/gateway/master/mpt.zip > v1.0.zip
+echo ""
 echo "Unzipping to local folder"
 unzip v1.0.zip
