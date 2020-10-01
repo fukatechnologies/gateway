ed205058649e8b3d152846f97f3c3c2aeba0d435
fukatechnologies <67410073+fukatechnologies@users.noreply.github.com>
2020-10-01 12:00:05 +0800

    Create installme.sh

---
+++ rpi/installme.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "Downloading to local folder"
+curl -k https://raw.githubusercontent.com/fukatechnologies/gateway/master/mpt.zip > v1.0.zip
+echo "Unzipping to local folder"
+unzip v1.0.zip
