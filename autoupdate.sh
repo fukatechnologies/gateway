92173a9db6cc811144549301782f3db4ffd097a4
fukatechnologies <67410073+fukatechnologies@users.noreply.github.com>
2020-10-02 22:03:29 +0800

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
-CUR_VER="0.0.1"
+CUR_VER="0.0.0.1"
 GEN_FILE=run.sh
 SCRIPT_LOCATION="${BASH_SOURCE[@]}"
 AUTO_UPDATE_URL='https://raw.githubusercontent.com/fukatechnologies/gateway/master/autoupdate.sh'
 
 function autoupdater()
 {
-    TMP_FILE=$(mktemp -p "" "XXXXX.sh")
-    curl -s -L "$AUTO_UPDATE_URL" > "$TMP_FILE"
-    NEW_VER=$(grep "^VERSION" "$TMP_FILE" | awk -F'[="]' '{print $3}')
+    RANDOM=$(mktemp -p "" "XXXXX.sh")
+    curl -s -L "$AUTO_UPDATE_URL" > "$RANDOM"
+    NEW_VER=$(grep "^CUR_VER" "$RANDOM" | awk -F'[="]' '{print $3}')
     if [ "$CUR_VER" \< "$NEW_VER" ]
     then
         printf "Updating from \e[31;1m%s\e[0m -> \e[32;1m%s\e[0m\n" "$CUR_VER" "$NEW_VER"
 
-	ALT_FILE=$(readlink -f "$SCRIPT_LOCATION")
-        echo "cp \"$TMP_FILE\" \"$ALT_FILE\"" > $GEN_FILE
-        echo "rm -f \"$TMP_FILE\"" >> $GEN_FILE
-        echo "echo Running script again: `basename ${BASH_SOURCE[@]}` $@" >> $GEN_FILE
+		ALT_FILE=$(readlink -f "$SCRIPT_LOCATION")
+        echo "cp \"$RANDOM\" \"$ALT_FILE\"" > $GEN_FILE
+        echo "rm -f \"$RANDOM\"" >> $GEN_FILE
+        echo "echo Attempt re-execution: `basename ${BASH_SOURCE[@]}` $@" >> $GEN_FILE
         echo "exec \"$ALT_FILE\" \"$@\"" >> $GEN_FILE
 
         chmod +x $GEN_FILE
-        chmod +x "$TMP_FILE"
+        chmod +x $RANDOM
         exec $GEN_FILE
     else
 	echo "Up to date."
-        rm -f "$TMP_FILE"
+        rm -f $RANDOM
     fi
 }
 
