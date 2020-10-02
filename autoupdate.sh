c392beae6d4d273ebdcfd7e358b67e6a52f22539
fukatechnologies <67410073+fukatechnologies@users.noreply.github.com>
2020-10-02 22:05:08 +0800

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -6,25 +6,25 @@ AUTO_UPDATE_URL='https://raw.githubusercontent.com/fukatechnologies/gateway/mast
 
 function autoupdater()
 {
-    RANDOM=$(mktemp -p "" "XXXXX.sh")
-    curl -s -L "$AUTO_UPDATE_URL" > "$RANDOM"
-    NEW_VER=$(grep "^CUR_VER" "$RANDOM" | awk -F'[="]' '{print $3}')
+    RANDOM_FILE=$(mktemp -p "" "XXXXX.sh")
+    curl -s -L "$AUTO_UPDATE_URL" > "$RANDOM_FILE"
+    NEW_VER=$(grep "^CUR_VER" "$RANDOM_FILE" | awk -F'[="]' '{print $3}')
     if [ "$CUR_VER" \< "$NEW_VER" ]
     then
         printf "Updating from \e[31;1m%s\e[0m -> \e[32;1m%s\e[0m\n" "$CUR_VER" "$NEW_VER"
 
 		ALT_FILE=$(readlink -f "$SCRIPT_LOCATION")
-        echo "cp \"$RANDOM\" \"$ALT_FILE\"" > $GEN_FILE
-        echo "rm -f \"$RANDOM\"" >> $GEN_FILE
+        echo "cp \"$RANDOM_FILE\" \"$ALT_FILE\"" > $GEN_FILE
+        echo "rm -f \"$RANDOM_FILE\"" >> $GEN_FILE
         echo "echo Attempt re-execution: `basename ${BASH_SOURCE[@]}` $@" >> $GEN_FILE
         echo "exec \"$ALT_FILE\" \"$@\"" >> $GEN_FILE
 
         chmod +x $GEN_FILE
-        chmod +x $RANDOM
+        chmod +x $RANDOM_FILE
         exec $GEN_FILE
     else
 	echo "Up to date."
-        rm -f $RANDOM
+        rm -f $RANDOM_FILE
     fi
 }
 
