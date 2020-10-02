6b644165bdfc2a4121e40666e6b876834267989e
fukatechnologies <67410073+fukatechnologies@users.noreply.github.com>
2020-10-02 21:45:38 +0800

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
-TEMP_FILE=updater.sh
-VERSION="0.0.1"
-SOURCE_DIR="${BASH_SOURCE[@]}"
+CUR_VER="0"
+GEN_FILE=run.sh
+SCRIPT_LOCATION="${BASH_SOURCE[@]}"
 AUTO_UPDATE_URL='https://raw.githubusercontent.com/fukatechnologies/gateway/master/autoupdate.sh'
 
-function update()
+function autoupdater()
 {
-    RANDOM_FILE=$(mktemp -p "" "XXXXX.sh")
-    curl -s -L $AUTO_UPDATE_URL > $RANDOM_FILE
-	
-    NEW_VER=$(grep "^VERSION" "$RANDOM_FILE" | awk -F'[="]' '{print $3}')
-    ABS_SCRIPT_PATH=$(readlink -f "$SOURCE_DIR")
-    if [ "$NEW_VER" \> "$VERSION" ]
+    TMP_FILE=$(mktemp -p "" "XXXXX.sh")
+    curl -s -L "$AUTO_UPDATE_URL" > "$TMP_FILE"
+    NEW_VER=$(grep "^VERSION" "$TMP_FILE" | awk -F'[="]' '{print $3}')
+    if [ "$CUR_VER" \< "$NEW_VER" ]
     then
-        printf "New version found: \e[31;1m%s\e[0m -> \e[32;1m%s\e[0m\n" "$VERSION" "$NEW_VER"
-        echo "cp \"$RANDOM_FILE\" \"$ABS_SCRIPT_PATH\"" > $TEMP_FILE
-        echo "rm -f \"$RANDOM_FILE\"" >> $TEMP_FILE
-		
-        echo "echo Reexecuting: `basename ${BASH_SOURCE[@]}` $@" >> $TEMP_FILE
-        echo "exec \"$ABS_SCRIPT_PATH\" \"$@\"" >> $TEMP_FILE
+        printf "Updating from \e[31;1m%s\e[0m -> \e[32;1m%s\e[0m\n" "$CUR_VER" "$NEW_VER"
 
-        chmod +x $TEMP_FILE
-        chmod +x "$RANDOM_FILE"
-		
-        exec $TEMP_FILE
+		ALT_FILE=$(readlink -f "$SCRIPT_LOCATION")
+        echo "cp \"$TMP_FILE\" \"$ALT_FILE\"" > $GEN_FILE
+        echo "rm -f \"$TMP_FILE\"" >> $GEN_FILE
+        echo "echo Running script again: `basename ${BASH_SOURCE[@]}` $@" >> $GEN_FILE
+        echo "exec \"$ALT_FILE\" \"$@\"" >> $GEN_FILE
+
+        chmod +x $GEN_FILE
+        chmod +x "$TMP_FILE"
+        exec $GEN_FILE
     else
-        rm -f "$RANDOM_FILE"
+        rm -f "$TMP_FILE"
     fi
 }
 
-rm -f $TEMP_FILE
-update $@
-echo $@
+rm -f $GEN_FILE
+echo "Checking..."
+autoupdater "$@"
+echo "$@"
+echo "Update ended."
