a5a1683fc3015bacba5d80934bc8f175bc7ec373
fukatechnologies <67410073+fukatechnologies@users.noreply.github.com>
2020-11-22 13:36:33 +0800

    Update updateFlow.sh

--- flows/updateFlow.sh
+++ flows/updateFlow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -kL GET https://raw.githubusercontent.com/fukatechnologies/gateway/master/flows/full.json -o updateFlow.json
+curl -kL https://raw.githubusercontent.com/fukatechnologies/gateway/master/flows/full.json -o updateFlow.json
 curl -vX POST http://localhost:1880/flows -d @updateFlow.json --header "Content-Type: application/json"
 rm updateFlow.json
