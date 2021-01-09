a69eeaa9a3bcebaa5b37b5298c5b4fe3968b0430
fukatechnologies <67410073+fukatechnologies@users.noreply.github.com>
2021-01-09 18:33:00 +0800

    Update installNodeRed.sh

--- rpi/installNodeRed.sh
+++ rpi/installNodeRed.sh
@@ -3,5 +3,5 @@
 sudo npm install -g --unsafe-perm node-red
 nohup node-red &
 curl -kL https://raw.githubusercontent.com/fukatechnologies/gateway/master/flows/firstrun.json -o updateFlow.json
-curl -vX POST http://localhost:1880/flows -d @updateFlow.json --header "Content-Type: application/json"
+curl -v POST http://localhost:1880/flows -d @updateFlow.json --header "Content-Type: application/json"
 rm updateFlow.json
