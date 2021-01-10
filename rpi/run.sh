d9409b843c5847c2879963a6d08407e8310fb6da
fukatechnologies <67410073+fukatechnologies@users.noreply.github.com>
2021-01-10 23:44:09 +0800

    Update run.sh

--- rpi/run.sh
+++ rpi/run.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-URL="https://raw.githubusercontent.com/fukatechnologies/gateway/master/rpi/installme.sh"
+URL="https://raw.githubusercontent.com/fukatechnologies/gateway/master/rpi/downloadAll.sh"
 rm -r fuka
 mkdir fuka
 cd fuka
 curl -kL $URL | bash -
 
 chmod 755 *
-sh runRpi.sh
