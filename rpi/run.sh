680cc12ed7ec26c433a7cc12f22e823641e5fef5
fukatechnologies <67410073+fukatechnologies@users.noreply.github.com>
2021-01-11 01:07:06 +0800

    Update run.sh

--- rpi/run.sh
+++ rpi/run.sh
@@ -2,7 +2,10 @@
 URL="https://raw.githubusercontent.com/fukatechnologies/gateway/master/rpi/downloadAll.sh"
 rm -r fuka
 mkdir fuka
+
+cd fuka
 curl -kL $URL | bash -s 'dailyrun.sh' 'installNode.sh'
 
+cd ..
 chmod 755 fuka/*
 source fuka/dailyrun.sh
