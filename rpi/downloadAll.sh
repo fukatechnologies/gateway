2d15e3854dc6e32dbf1d558420b17de2bfa4803d
fukatechnologies <67410073+fukatechnologies@users.noreply.github.com>
2021-01-10 23:42:21 +0800

    Create downloadAll.sh

---
+++ rpi/downloadAll.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DUMP="https://raw.githubusercontent.com/fukatechnologies/gateway/master/rpi"
+FILES=("dailyrun.sh" "installNode.sh")
+
+DownloadFile () {
+   echo "Downloading $2"
+   curl -k $1/$2 > $2
+   echo ""
+   return 0
+}
+
+echo "Downloading to local folder"
+for file in ${FILES[*]}; do
+  DownloadFile $DUMP $file
+done
