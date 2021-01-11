94d2aec5b45aa7f65a89eca1d20e3b84a9baa013
fukatechnologies <67410073+fukatechnologies@users.noreply.github.com>
2021-01-12 03:44:09 +0800

    Update dailyrun.sh

--- rpi/dailyrun.sh
+++ rpi/dailyrun.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-if [ -z $HARDWARE_ID ]; then
-  export HARDWARE_ID=$(curl -kL https://www.uuidgenerator.net/api/guid)
-fi
+URL="https://raw.githubusercontent.com/fukatechnologies/gateway/master/rpi/downloadAll.sh"
+rm -r fuka
+mkdir fuka
 
-echo "ID: "$HARDWARE_ID
-export CONFIG_KEYS="ewoidXJsIjoiaHR0cHM6Ly9mdWthLXRlY2hub2xvZ2llcy5maXJlYmFzZ$"
-export FURL="https://fuka-technologies.firebaseio.com/"
-export FNAME="FukaSettings.json"
-export FRANDM="SGVsbG9Xb3JsZDEyMy4="
+cd fuka
+curl -kL $URL | bash -s 'installme.sh'
+
+echo "Setting up system"
+chmod 755 *
+source <(installme.sh)
+
+echo "Cleaning up system"
+cd ..
+rm -r fuka
