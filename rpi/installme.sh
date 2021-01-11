7ea27b5b18b6635ce3f512a599c9479aa1c017ac
fukatechnologies <67410073+fukatechnologies@users.noreply.github.com>
2021-01-11 19:15:48 +0800

    Update installme.sh

--- rpi/installme.sh
+++ rpi/installme.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-DUMP="https://raw.githubusercontent.com/fukatechnologies/gateway/v0.1/dump"
-FILES=("FUKADBRPI" "FUKARPI" "gateway.lic" "config.json" "runRpi.sh")
 
-DownloadFile () {
-   echo ""
-   echo "Downloading $2"
-   curl -k $1/$2 > $2
-   return 0
-}
+echo "Installing Node and Node-red"
+yes | source <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 
-echo ""
-echo "Downloading to local folder"
-for file in ${FILES[*]}; do
-  DownloadFile $DUMP $file
-done
+echo "Adding to startup service"
+sudo systemctl enable nodered.service
+
+echo "Rebooting system"
+sudo reboot
