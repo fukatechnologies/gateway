bed6147f9afd338f2d3fe6a2453be50c0b97aa8a
fukatechnologies <67410073+fukatechnologies@users.noreply.github.com>
2021-01-12 03:51:11 +0800

    Update run.sh

--- rpi/run.sh
+++ rpi/run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo "Installing..."
-source <(curl -kL https://raw.githubusercontent.com/fukatechnologies/gateway/master/rpi/dailyrun.sh)
-
 echo "Update system parameters"
 export CONFIG_KEYS="ewoidXJsIjoiaHR0cHM6Ly9mdWthLXRlY2hub2xvZ2llcy5maXJlYmFzZ$"
 export FURL="https://fuka-technologies.firebaseio.com/"
@@ -12,5 +9,9 @@ if [ -z $HARDWARE_ID ]; then
   export HARDWARE_ID=$(curl -kL https://www.uuidgenerator.net/api/guid)
 fi
 echo "ID: $HARDWARE_ID"
+
+echo "Installing..."
+source <(curl -kL https://raw.githubusercontent.com/fukatechnologies/gateway/master/rpi/dailyrun.sh)
+
 echo "Rebooting system"
 sudo reboot
