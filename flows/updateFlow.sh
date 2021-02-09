8408d773fae364fc2521ad1469a3743e00c1c55b
adriankhoo <46496426+adriankhoo@users.noreply.github.com>
2021-02-10 01:48:35 +0800

    Update updateFlow.sh

--- flows/updateFlow.sh
+++ flows/updateFlow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -kL https://github.com/fukatechnologies/gateway/blob/master/flows/firstrun.json -o updateFlow.json
+curl -kL https://raw.githubusercontent.com/fukatechnologies/gateway/master/flows/firstrun.json -o updateFlow.json
 curl -vX POST http://localhost:1880/flows -d @updateFlow.json --header "Content-Type: application/json"
 rm updateFlow.json
