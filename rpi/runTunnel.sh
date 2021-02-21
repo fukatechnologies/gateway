2ab371b4859a014e2681183b94e4f37691ad0392
fukatechnologies <67410073+fukatechnologies@users.noreply.github.com>
2021-02-21 19:13:25 +0800

    Update runTunnel.sh
    Added download for rpi

--- rpi/runTunnel.sh
+++ rpi/runTunnel.sh
@@ -4,48 +4,63 @@ NOHUPFILE="nohup.out"
 LOGFILE="tcp.ngrok.log"
 
 PID=$(ps | grep ngrok | grep -v grep | head -n 1 | awk '{print $1}')
-if [ "$1" == "pid" ]; then
-    echo "$PID"
-    exit 0
-elif [ "$1" == "ssh" ]; then
-    if test -f "$LOGFILE"; then
-        echo "Old instance running"
+if test -f "ngrok"; then
+    if [ "$1" == "pid" ]; then
+        if [ -z "$PID" ]; then
+            echo "No instance"
+        else
+            echo "$PID"
+        fi
+        exit 0
+    elif [ "$1" == "ssh" ]; then
+        if test -f "$LOGFILE"; then
+            echo "Service running: $PID"   
+            tail $LOGFILE | grep 'url=' | awk -F'url=' '{print $2}'
+            exit 0
+        else
+            nohup ./ngrok tcp 3389 --log="$LOGFILE" --authtoken=$TOKEN &
+            echo "Wait few seconds to establish connection."
+        fi 
+    elif [ "$1" == "rdp" ]; then 
+        if test -f "$LOGFILE"; then
+            echo "Service running: $PID"   
+            tail $LOGFILE | grep 'url=' | awk -F'url=' '{print $2}'
+            exit 0
+        else
+            nohup ./ngrok tcp 3389 --log="$LOGFILE" --authtoken=$TOKEN &
+            echo "Wait few seconds to establish connection."
+        fi 
+    elif [ "$1" == "stop" ]; then
+        if [ -z "$PID" ]; then
+            echo "Service stopped."
+        else
+            echo "Killing $PID"
+            kill -9 $PID
+        fi
     else
-        nohup ./ngrok tcp 3389 --log="$LOGFILE" --authtoken=$TOKEN &
-        echo "Wait few seconds to establish connection."
-    fi 
-elif [ "$1" == "rdp" ]; then 
-    if test -f "$LOGFILE"; then
-        echo "Old instance running"
-    else
-        nohup ./ngrok tcp 3389 --log="$LOGFILE" --authtoken=$TOKEN &
-        echo "Wait few seconds to establish connection."
-    fi 
-elif [ "$1" == "stop" ]; then
+        echo "Command not supported."
+        exit 0
+    fi
+
+    PID=$(ps | grep ngrok | grep -v grep | head -n 1 | awk '{print $1}')
     if [ -z "$PID" ]; then
-        echo "Service stopped."
-    else
-        echo "Killing $PID"
-        kill -9 $PID
+        if test -f "$NOHUPFILE"; then
+            echo "Attempt delete $NOHUPFILE"
+            rm $NOHUPFILE
+        fi 
+
+        if test -f "$LOGFILE"; then
+            echo "Attempt delete $LOGFILE"
+            rm $LOGFILE
+            echo "Service stopped."
+        fi
+    elif test -f "$LOGFILE"; then 
+        echo "Service running: $PID"   
+        tail $LOGFILE | grep 'url=' | awk -F'url=' '{print $2}'
     fi
 else
-    echo "Command not supported."
-    exit 0
-fi
-
-PID=$(ps | grep ngrok | grep -v grep | head -n 1 | awk '{print $1}')
-if [ -z "$PID" ]; then
-    if test -f "$NOHUPFILE"; then
-        echo "Attempt delete $NOHUPFILE"
-        rm $NOHUPFILE
-    fi 
-
-    if test -f "$LOGFILE"; then
-        echo "Attempt delete $LOGFILE"
-        rm $LOGFILE
-        echo "Service stopped."
+    if [ "$1" == "download" ]; then
+        curl -kL https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip --output ngrok_dl.zip
+        unzip ngrok_dl.zip
     fi
-elif test -f "$LOGFILE"; then 
-    echo "Service running: $PID"   
-    tail $LOGFILE | grep 'url=' | awk -F'url=' '{print $2}'
 fi
